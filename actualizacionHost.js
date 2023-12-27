5e1cd6de46ab430f3bde60b3fd1a00751888490c
jumunozdev <jmmunozpu@unadvirtual.edu.co>
2023-12-27 13:05:16 -0500

    Se agrega funcion para actualizar los host a partir de un excel

--- actualizacionHost.js
+++ actualizacionHost.js
@@ -1,40 +1,61 @@
-const config = require('config.js');
+const config = require('./config.js');
 const axios = require('axios');
 const XLSX = require('xlsx');
 
 const { urlApi, tokenApi } = config;
 
-async function actualizarHosts(archivoExcel, urlApi, tokenApi) {
-    const workbook = XLSX.readFile(archivoExcel);
-    const sheetNameList = workbook.SheetNames;
-    const sheet = workbook.Sheets[sheetNameList[0]];
-    const data = XLSX.utils.sheet_to_json(sheet);
+// Lee el archivo Excel
+function readExcel(filePath) {
+  const workbook = XLSX.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
 
-    for (let i = 0; i < data.length; i++) {
-        const host = data[i];
-
-        try {
-            const response = await axios({
-                method: 'post',
-                url: urlApi,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                data: {
-                    jsonrpc: '2.0',
-                    method: 'host.update',
-                    params: {
-                        hostid: host.hostid,
-                        host: host.host,
-                    },
-                    auth: tokenApi,
-                    id: 1,
-                },
-            });
-
-            console.log(`Host ${host.host} actualizado con éxito.`);
-        } catch (error) {
-            console.error(`Error al actualizar el host ${host.host}: ${error}`);
-        }
-    }
+  return XLSX.utils.sheet_to_json(sheet);
 }
+
+// Actualiza hosts en Zabbix
+async function updateHosts(tokenApi, excelData) {
+  const headers = { 'Content-Type': 'application/json' };
+
+  for (const row of excelData) {
+    const updateData = {
+      jsonrpc: '2.0',
+      method: 'host.update',
+      params: {
+        hostid: row.hostid,
+        inventory: {
+          location_lat: row.location_lat,
+          location_lon: row.location_lon,
+          site_city: row.site_city,
+          site_state: row.site_state,
+        },
+      },
+      auth: tokenApi,
+      id: 1,
+    };
+
+    try {
+      const response = await axios.post(urlApi, updateData, { headers });
+      const result = response.data;
+
+      console.log(`Resultado para hostid ${row.hostid}:`, result);
+    } catch (error) {
+      console.error(`Error al actualizar hostid ${row.hostid}:`, error.message);
+    }
+  }
+}
+
+// Ejecuta el proceso
+async function main() {
+  try {
+    
+    const excelData = readExcel('./data/prueba.xlsx');
+    await updateHosts(tokenApi, excelData);
+  } catch (error) {
+    console.error(error.message);    
+  }
+}
+
+
+// Inicia el proceso principal
+main();
